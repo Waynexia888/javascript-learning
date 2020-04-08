95119052cbd9cbf77b5ed2f609bb81d2d7295557
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-08 03:21:14 -0700

    updated

---
+++ jobchat/前后端联调/前后端联调.js
@@ -0,0 +1,11 @@
+// 前后端联调 -> 使用axios发送异步请求
+// - 如何发送，端口不一致，使用proxy配置转发
+// - axios有拦截器这个概念，统一loading处理
+// - redux里使用异步数据，渲染页面
+
+// axios
+// - 简介好用的发送请求库，npm install axios --save 安装
+// - Axios.interceptors设置拦截器，比如全局的loading
+// - axios.get, axios.post 发送请求，返回promise对象
+// - redux里获取数据， 然后dispatch即可
+
