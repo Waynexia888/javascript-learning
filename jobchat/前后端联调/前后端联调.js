e6f31110a058fceeb934576e1a67ec08dfed2281
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-08 18:33:44 -0700

    updated

--- jobchat/前后端联调/前后端联调.js
+++ jobchat/前后端联调/前后端联调.js
@@ -9,3 +9,8 @@
 // - axios.get, axios.post 发送请求，返回promise对象
 // - redux里获取数据， 然后dispatch即可
 
+// 开发模式：
+// 基于cookie用户验证
+// - express依赖cookie-parser，需要npm install cookie-parser --save 安装
+// - cookie 类似于一张身份卡，登陆后服务器端返回，你带着cookie就可以访问受限资源
+// - 页cookie的管理浏览器会自动处理
