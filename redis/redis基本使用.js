3aa38e0fdcb1212f9fa4d25021dfd2b683fe772e
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-23 17:06:40 -0700

    updated

---
+++ redis/redis基本使用.js
@@ -0,0 +1,19 @@
+// 内存数据结构存储
+// 可持久存储
+// 支持多种数据结构
+// redis-cli =>通过这个commend进行数据库连接
+// 密码登录 
+
+// set b 111 => OK
+// get b => "111"
+// setex c 10 1 c这个key，值为1， 10秒钟后自动删除（过期）
+// gey c => 1
+// get c => nil (10秒后变为nil)
+
+// redis是基于key,value来存储的,不像mysql是有table概念的，
+// 所以为了避免后面的key跟前面的key重合（覆盖）,可以在前面加一个模块（例如：session）
+// set session:sessionId 123
+// get session:sessionId => "123"
+
+// KEYs *   => 查找所有的key
+// DEL a    => 删除key为a的这个key
